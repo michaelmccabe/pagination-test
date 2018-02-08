079973763f21d1659219a1add2a1e6d02f1019e1
Michael McCabe <mccmichael@gmail.com>
2018-02-08 12:22:14 +0000

    added tests for RevenueCalculator

---
+++ src/main/java/com/mic/document/service/exceptions/RevenueException.java
@@ -0,0 +1,9 @@
+package com.mic.document.service.exceptions;
+
+public class RevenueException extends Exception {
+
+	public RevenueException(String string) {
+		super(string);
+	}
+
+}
--- src/test/java/com/mic/document/service/RevenueCalculatorTest.java
+++ src/test/java/com/mic/document/service/RevenueCalculatorTest.java
@@ -1,12 +1,20 @@
 package com.mic.document.service;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
 
 import java.math.BigDecimal;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import com.mic.document.service.exceptions.RevenueException;
 
 public class RevenueCalculatorTest {
+	
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
 	@Test
 	public void testRevenue() throws Exception {
@@ -14,4 +22,30 @@ public class RevenueCalculatorTest {
 		BigDecimal revenue = RevenueCalculator.calculateRevenue(new BigDecimal("0.2"), new BigDecimal("400"));
 		assertEquals(new BigDecimal("500.00"), revenue);
 	}
+	
+	@Test
+	public void testNegativeCostOfGoods() throws Exception {
+
+		expectedException.expect(RevenueException.class);
+		expectedException.expectMessage(containsString("Cost Of Goods must be greater than zero"));		
+		
+		RevenueCalculator.calculateRevenue(new BigDecimal("0.2"), new BigDecimal("-400"));
+		
+	}
+	
+	@Test
+	public void testNegativePercentage() throws Exception {
+		
+		BigDecimal revenue = RevenueCalculator.calculateRevenue(new BigDecimal("-0.2"), new BigDecimal("400"));
+		assertEquals(new BigDecimal("333.33"), revenue);		
+		
+	}
+		
+	@Test
+	public void testZeroCostOfGoods() throws Exception {
+		
+		BigDecimal revenue = RevenueCalculator.calculateRevenue(new BigDecimal("0.2"), new BigDecimal("0.00"));
+		assertEquals(new BigDecimal("0.00"), revenue);		
+		
+	}	
 }
